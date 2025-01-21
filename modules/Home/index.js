d45b61a4044a87cdba5f666d513c6a3e842b6d34
jay waghode <jaywaghode37@gmail.com>
2025-01-21 20:51:14 +0530

    favicon & moved data to json

--- modules/Home/index.js
+++ modules/Home/index.js
@@ -8,108 +8,16 @@ import classNames from "classnames";
  * Internal dependencies
  */
 import TabIcon from "ece/components/tab-icon";
+import ProductCard from "ece/components/product-card";
+import products from "./list.json";
 
 /**
  * Styles dependencies
  */
 import styles from "./index.module.css";
-import ProductCard from "ece/components/product-card";
 
 function HomePage() {
   const [visible, setVisible] = useState(false);
-
-  const products = [
-    {
-      id: 1,
-      name: "Vicent Change Online",
-      brand_name: "Eye Care Expert",
-      original_price: 2000,
-      discounted_percentage: 60,
-      final_price: 800,
-      images: ["./images/demo.png", "./images/demo.png", "./images/demo.png"],
-    },
-    {
-      id: 2,
-      name: "Vicent Change Online",
-      brand_name: "Eye Care Expert",
-      original_price: 2000,
-      discounted_percentage: 60,
-      final_price: 800,
-      images: ["./images/demo.png", "./images/demo.png", "./images/demo.png"],
-    },
-    {
-      id: 3,
-      name: "Vicent Change Online",
-      brand_name: "Eye Care Expert",
-      original_price: 2000,
-      discounted_percentage: 60,
-      final_price: 800,
-      images: ["./images/demo.png", "./images/demo.png", "./images/demo.png"],
-    },
-    {
-      id: 4,
-      name: "Vicent Change Online",
-      brand_name: "Eye Care Expert",
-      original_price: 2000,
-      discounted_percentage: 60,
-      final_price: 800,
-      images: ["./images/demo.png", "./images/demo.png", "./images/demo.png"],
-    },
-    {
-      id: 5,
-      name: "Vicent Change Online",
-      brand_name: "Eye Care Expert",
-      original_price: 2000,
-      discounted_percentage: 60,
-      final_price: 800,
-      images: ["./images/demo.png", "./images/demo.png", "./images/demo.png"],
-    },
-    {
-      id: 6,
-      name: "Vicent Change Online",
-      brand_name: "Eye Care Expert",
-      original_price: 2000,
-      discounted_percentage: 60,
-      final_price: 800,
-      images: ["./images/demo.png", "./images/demo.png", "./images/demo.png"],
-    },
-    {
-      id: 7,
-      name: "Vicent Change Online",
-      brand_name: "Eye Care Expert",
-      original_price: 2000,
-      discounted_percentage: 60,
-      final_price: 800,
-      images: ["./images/demo.png", "./images/demo.png", "./images/demo.png"],
-    },
-    {
-      id: 8,
-      name: "Vicent Change Online",
-      brand_name: "Eye Care Expert",
-      original_price: 2000,
-      discounted_percentage: 60,
-      final_price: 800,
-      images: ["./images/demo.png", "./images/demo.png", "./images/demo.png"],
-    },
-    {
-      id: 9,
-      name: "Vicent Change Online",
-      brand_name: "Eye Care Expert",
-      original_price: 2000,
-      discounted_percentage: 60,
-      final_price: 800,
-      images: ["./images/demo.png", "./images/demo.png", "./images/demo.png"],
-    },
-    {
-      id: 10,
-      name: "Vicent Change Online",
-      brand_name: "Eye Care Expert",
-      original_price: 2000,
-      discounted_percentage: 60,
-      final_price: 800,
-      images: ["./images/demo.png", "./images/demo.png", "./images/demo.png"],
-    },
-  ];
 
   return (
     <>
