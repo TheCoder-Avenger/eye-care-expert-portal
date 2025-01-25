6caa9cd9dd76946412a32e9634caaca8f121efc5
jay waghode <jaywaghode37@gmail.com>
2025-01-25 17:12:26 +0530

    updates fixes

--- modules/Home/index.js
+++ modules/Home/index.js
@@ -26,11 +26,10 @@ function HomePage() {
     setChecked(checked);
   };
 
-  return (
-    <>
-      <div className={styles["header"]}>
-        <img src="./images/logo.jpeg" className={styles["logo"]} />
-        {/* <div>
+  const renderHeader = () => (
+    <div className={styles["header"]}>
+      <img src="./images/logo-trans2.png" className={styles["logo"]} />
+      {/* <div>
           <Switch
             onChange={handleChange}
             checked={checked}
@@ -39,74 +38,98 @@ function HomePage() {
           />
           <div className={styles["product-type"]}>(Sunglasses)</div>
         </div> */}
-        <TabIcon
-          icon="menu-2"
-          color={"#FFF"}
-          onClick={() => setVisible(true)}
-        />
+      <TabIcon icon="menu-2" color={"#bwd"} onClick={() => setVisible(true)} />
+    </div>
+  );
+
+  const renderDrawer = () => (
+    <div
+      className={classNames(styles["drawer-wrapper"], {
+        [styles["drawer-open"]]: visible,
+      })}
+    >
+      <div className={styles["contact-us"]}>
+        <a
+          href={`https://wa.me/919653181580?text=${encodeURI(
+            "i am interested in your sunglasses and eyeglasses"
+          )}`}
+          target="_blank"
+        >
+          Click here to contact us
+        </a>
       </div>
-      <div
-        className={classNames(styles["drawer-wrapper"], {
-          [styles["drawer-open"]]: visible,
-        })}
-      >
-        <div className={styles["contact-us"]}>
-          <a
-            href={`https://wa.me/919653181580?text=${encodeURI(
-              "i am interested in your sunglasses and eyeglasses"
-            )}`}
-            target="_blank"
-          >
-            Contact Us
-          </a>
+
+      <img src="./images/logo.jpeg" />
+      <div className={styles["comming-soon"]}>Coming Soon...</div>
+      <div className={styles["contact-person"]}>
+        Contact Us (Rahim Shaikh) <br /> +91 8879046890
+      </div>
+      <TabIcon
+        icon="x"
+        className={styles["drawer-close"]}
+        onClick={() => setVisible(false)}
+      />
+    </div>
+  );
+
+  const renderFilterAndSortOptions = () => (
+    <div className={styles["filter-sort-wrapper"]}>
+      <div className={styles["filter-sort-sub-wrapper"]}>
+        <div
+          className={styles["sort-wrapper"]}
+          onClick={() => setShowFilter(true)}
+        >
+          <TabIcon icon="arrows-sort" />
+          <div>SORT</div>
         </div>
-        <img src="./images/logo.jpeg" />
-        <div className={styles["comming-soon"]}>Coming Soon</div>
-        <TabIcon
-          icon="x"
-          className={styles["drawer-close"]}
-          onClick={() => setVisible(false)}
-        />
-      </div>
-      <div className={styles["filter-sort-wrapper"]}>
-        <div className={styles["filter-sort-sub-wrapper"]}>
-          <div
-            className={styles["sort-wrapper"]}
-            onClick={() => setShowFilter(true)}
-          >
-            <TabIcon icon="arrows-sort" />
-            <div>SORT</div>
-          </div>
-          <div className={styles["filter-divider"]}></div>
-          <div
-            className={styles["filter-wrapper"]}
-            onClick={() => setShowFilter(true)}
-          >
-            <TabIcon icon="adjustments-horizontal" />
-            <div>FILTER</div>
-          </div>
+        <div className={styles["filter-divider"]}></div>
+        <div
+          className={styles["filter-wrapper"]}
+          onClick={() => setShowFilter(true)}
+        >
+          <TabIcon icon="adjustments-horizontal" />
+          <div>FILTER</div>
         </div>
       </div>
-      <div
-        className={classNames(styles["filter-section-wrapper"], {
-          [styles["filter-section-open"]]: showFilter,
-        })}
-      >
-        <img src="./images/logo.jpeg" className={styles["image"]} />
-        <div className={styles["comming-soon"]}>Coming Soon</div>
-        <TabIcon
-          icon="x"
-          className={styles["drawer-close"]}
-          onClick={() => setShowFilter(false)}
-        />
+    </div>
+  );
+
+  const renderFilterAndSortModal = () => (
+    <div
+      className={classNames(styles["filter-section-wrapper"], {
+        [styles["filter-section-open"]]: showFilter,
+      })}
+    >
+      <img src="./images/logo.jpeg" className={styles["image"]} />
+      <div className={styles["comming-soon"]}>Coming Soon...</div>
+      <div className={styles["contact-person"]}>
+        Contact Us (Rahim Shaikh) <br /> +91 8879046890
       </div>
-      <div className={styles["container"]}>
-        {products.map((item, key) => (
-          <div key={key}>
-            <ProductCard item={item} />
-          </div>
-        ))}
-      </div>
+      <TabIcon
+        icon="x"
+        className={styles["drawer-close"]}
+        onClick={() => setShowFilter(false)}
+      />
+    </div>
+  );
+
+  const renderProducts = () => (
+    <div className={styles["container"]}>
+      {products.map((item, key) => (
+        <div key={key}>
+          <ProductCard item={item} />
+        </div>
+      ))}
+    </div>
+  );
+
+  return (
+    <>
+      {renderHeader()}
+      {renderDrawer()}
+      {renderFilterAndSortOptions()}
+      {renderFilterAndSortModal()}
+      {renderProducts()}
     </>
   );
 }
